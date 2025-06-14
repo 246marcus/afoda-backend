4696255a25c568722d156bf5f20d3cc43af80c5b
MARCUS <151760903+246marcus@users.noreply.github.com>
2025-06-14 19:20:52 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -19,7 +19,7 @@ app.use(cookieParser());
 // CORS Configuration
 app.use(
   cors({
-    origin: "http://localhost:5173", // or your frontend deployed domain
+    origin: "http://localhost:8080", // or your frontend deployed domain
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
