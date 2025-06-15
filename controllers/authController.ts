ef4a43245d5dcad5af5256a4ae3d46a3a3e4f94f
Adewale Marcus <adewaleibukun246@gmail.com>
2025-06-15 15:45:22 +0100

    went back

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -82,7 +82,6 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
       httpOnly: true,
       secure: true,
       sameSite: "none",
-      domain: ".afoda.store",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     };
 
